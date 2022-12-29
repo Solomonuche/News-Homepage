ab5ab8e3c254c3186047ccb33a025a070621b78d
Solomonuche <solomonuche@gmail.com>
2022-12-29 07:51:56 -0800

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+function displayMenu () {
+  let menu = document.getElementById("mobilemenu")
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+
+
